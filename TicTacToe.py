6dbb9d071914f8d62f92a589d196d93da5cf03c3
clementreiffers <reiffersclement@gmail.com>
2022-07-18 16:34:25 +0200

    ajout de regles

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,3 +1,7 @@
+def is_winner_in_list(list, player):
+    return any(line == [player] * 3 for line in list)
+
+
 class TicTacToe:
     def __init__(self):
         self.boardGame = [["-" for _ in range(3)] for _ in range(3)]
@@ -25,18 +29,15 @@ class TicTacToe:
                 print(f"| {value} ", end="")
             print("|")
 
-    def is_winner_in_list(self, list, player):
-        return any(line == [player] * 3 for line in list)
-
     def is_winner(self, player):
-        is_line_winner = self.is_winner_in_list(self.boardGame, player)
-        is_column_winner = self.is_winner_in_list(
+        is_line_winner = is_winner_in_list(self.boardGame, player)
+        is_column_winner = is_winner_in_list(
             [[line[column] for line in self.boardGame] for column in range(3)], player
         )
-        is_left_to_right_diagonal_winner = self.is_winner_in_list(
+        is_left_to_right_diagonal_winner = is_winner_in_list(
             [[line[column] for column, line in enumerate(self.boardGame)]], player
         )
-        is_right__to_left_diagonal_winner = self.is_winner_in_list(
+        is_right__to_left_diagonal_winner = is_winner_in_list(
             [[line[-column - 1] for column, line in enumerate(self.boardGame)]], player
         )
         return (
@@ -47,6 +48,11 @@ class TicTacToe:
         )
 
     def play(self, player):
-        x = int(input("give x coordinate between 0 and 2 \n>>>"))
-        y = int(input("give y coordinate between 0 and 2 \n>>>"))
+        playable = False
+        while not playable:
+            x = int(input("give x coordinate between 0 and 2 \n>>>"))
+            y = int(input("give y coordinate between 0 and 2 \n>>>"))
+            playable = 0 <= x <= 2 and 0 <= y <= 2 and self.boardGame[x][y] != "-"
+            if not playable:
+                print("NOT PLAYABLE : you must give (x, y) between 0 and 2")
         self.boardGame[x][y] = player
